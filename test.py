65b62e4352298cf4adaecefa428c0fc737b745fb
Eziotic <62868327+Eziotic@users.noreply.github.com>
2021-10-02 21:55:48 +0800

    yes

---
+++ test.py
@@ -0,0 +1,34 @@
+import xlwings as xw
+
+# app = xw.App(visible=False, add_book=False)
+# app.display_alerts=False
+# app.screen_updating=False
+# wb = app.books.add()
+# wb.sheets['sheet1'].range('A1').value='IT'
+# wb.sheets[0].range(2,1).value='小酒馆'
+# wb.save(r'Test.xlsx')
+# wb.close()
+# app.quit()
+# app = xw.App(visible=False, add_book=False)
+# app.display_alerts=False
+# app.screen_updating=False 
+# wb = app.books.open(r'Test.xlsx')
+# print(wb.sheets['sheet1'].range('A1:B1').value)
+
+
+# sht = wb.sheets[0]
+# sht.range('A1').value='10000'
+# a=sht.range('A1').value
+# print(a)
+# sht.range('A1').options(transpose=True).value=[1,2,3]
+# a=sht.range('A1:A3').value
+# print(a)
+# sht.range('A1').options(expand='table').value=[[1,2],[3,4]]
+# a=sht.range('A1:B2').value
+# print(a)
+
+# wb.save()
+# wb.close()
+# app.quit()
+
+#Book工作簿常用API
